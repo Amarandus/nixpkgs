a6701a7d743f6ba6edd8cda41a3ca696e637f719
CRTified <carl.schneider+github@ruhr-uni-bochum.de>
2022-07-11 19:56:26 +0200

    rrsync: Change dependency from perl to python3
    With rsync v3.2.4, rrsync has been migrated from perl to python3.
    This fixes #181096.

--- pkgs/applications/networking/sync/rsync/rrsync.nix
+++ pkgs/applications/networking/sync/rsync/rrsync.nix
@@ -1,10 +1,13 @@
-{ lib, stdenv, fetchurl, perl, rsync, fetchpatch }:
+{ lib, stdenv, fetchurl, python3, rsync, fetchpatch }:
 
 stdenv.mkDerivation {
   pname = "rrsync";
   inherit (rsync) version srcs;
 
-  buildInputs = [ rsync perl ];
+  buildInputs = [
+    rsync
+    (python3.withPackages (pythonPackages: with pythonPackages; [ braceexpand ]))
+  ];
 
   # Skip configure and build phases.
   # We just want something from the support directory
